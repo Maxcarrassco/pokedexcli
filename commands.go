4be70338cb5d906f7e5dedd8e0a4d9c9a2cd74f9
Maxwell D. Dorliea <deboy170@gmail.com>
2023-09-01 11:50:01 +0000

    Implement the catch, inspect, and pokedex commands of pokedexcli

--- commands.go
+++ commands.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"errors"
 	"fmt"
 	"os"
@@ -27,11 +28,26 @@ var Mapping = map[string]models.Command{
 		Description: "Lists all the Pokémon in a given area",
 		CallbackFn: commandExplore,
 	},
+	"catch": models.Command{
+		Description: "Catch a Pokemon and add it to the user's Pokedex",
+		CallbackFn: commandCatch,
+	},
+	"inspect": models.Command{
+		Description: "Print the name, height, weight, stats and type(s) of the Pokemon",
+		CallbackFn: commandInspect,
+	},
+	"pokedex": models.Command{
+		Description: "Print the name of Pokemon the user have caught",
+		CallbackFn: commandPokedex,
+	},
 }
+
+var UserPokemon = make(map[string]models.PokedexCatch)
 
 
 var pokedexLocation = models.PokedexLocation{}
 var pokedexLocationArea = models.PokedexLocationArea{}
+var pokedexCatch = models.PokedexCatch{}
 
 func commandExit(args string) error {
 	fmt.Println("Thanks for playing Pokedex")
@@ -95,6 +111,61 @@ func commandExplore(args string) error {
 	fmt.Println("Found Pokemon:")
 	for _, v := range pokedexLocationArea.PokemonEncounters {
 		fmt.Println(" - ", v.Pokemon.Name)
+	}
+	return nil
+}
+
+func commandCatch(args string) error {
+	poke := strings.Split(args, " ")
+	if len(poke) != 2 {
+		return errors.New("Not enough arguments passed")
+	}
+	url := fmt.Sprintf("%s/%s/%s", internal.BASE_URL, "pokemon", poke[1])
+	fmt.Printf("Throwing a Pokeball at %s...\n", poke[1])
+	err := internal.GetRequest(url, &pokedexCatch)
+	if err != nil {
+		return err
+	}
+	exp := pokedexCatch.BaseExperience
+	guess := rand.Int() % exp + 10
+	if guess == exp {
+		UserPokemon[poke[1]] = pokedexCatch
+		fmt.Println(poke[1], "was caught!")
+	} else {
+		fmt.Println(poke[1], "escaped!")
+	}
+	return nil
+}
+
+func commandInspect(args string) error {
+	poke := strings.Split(args, " ")
+	if len(poke) != 2 {
+		return errors.New("Not enough arguments passed")
+	}
+	if v, ok := UserPokemon[poke[1]]; ok {
+		fmt.Println("Name:", poke[1])
+		fmt.Println("Height:", v.Height)
+		fmt.Println("Weight:", v.Weight)
+
+		fmt.Println("Stats:")
+		for _, v := range v.Stats {
+			fmt.Printf(" - %s: %d\n", v.Stat.Name, v.BaseStat)
+		}
+		fmt.Println("Types:")
+		for _, v := range v.Types {
+			fmt.Printf(" - %s\n", v.Type.Name)
+		}
+	} else {
+		fmt.Println("you have not caught that pokemon")
+	}
+	return nil
+}
+
+
+func commandPokedex(args string) error {
+	fmt.Println("Your Pokedex:")
+	for k, _ := range UserPokemon {
+		fmt.Println(" - ", k)
 	}
 	return nil
 }
